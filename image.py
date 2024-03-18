db59ef637eb80e0ae2bebf49d40dc32ba78a84a2
Deividas Balandis <deividas.balandis@gmail.com>
2024-03-18 22:53:25 +0200

    Moving image processing to separate functions. Adding additional requirements

--- image.py
+++ image.py
@@ -1,22 +1,27 @@
+from PIL import Image, ImageFilter 
+from skimage.util import random_noise, img_as_float
+import numpy as np
 
+def apply_blur(image):
+  return image.filter(ImageFilter.GaussianBlur)
+
+def apply_black_and_white(image: Image.Image):
+  gray_scale = image.convert('L')
+  pixels = list(gray_scale.getdata())
+  pixels.sort()
+  thresh = pixels[len(pixels) // 2]
+  pixels_to_replace = sum(1 for pixel in pixels if pixel < thresh) / len(pixels) * 0.1
+
+  fn = lambda x : 255 if x > thresh else 0
+  return gray_scale.point(fn, mode='1'), pixels_to_replace
+
+def apply_noise(image: Image.Image, noise_pxl_count: float):
+  noised_image = random_noise(img_as_float(image), mode='s&p', amount=noise_pxl_count)
+  return Image.fromarray((noised_image * 255).astype(np.uint8))
 
 if __name__ == '__main__':
-  from PIL import Image, ImageFilter 
-  from skimage.util import random_noise, img_as_float
-  import numpy as np
-
-  # Opening the image  
-  # (R prefixed to string in order to deal with '\' in paths) 
   image = Image.open(r"Images/667626_18933d713e.jpg") 
-    
-  blur_image = image.filter(ImageFilter.GaussianBlur) 
-    
-  blur_image.save('blurred.jpg')
-
-  thresh = 150
-  fn = lambda x : 255 if x > thresh else 0
-  r = image.convert('L').point(fn, mode='1')
-  r.save('blacked.png')
-
-  noised_image = random_noise(img_as_float(image), mode='gaussian',mean=0.15)
-  Image.fromarray((noised_image * 255).astype(np.uint8)).save('noised.jpg')
+  blacked_image, noise_pxl_count = apply_black_and_white(image)
+  blacked_image.save("blacked_image.jpg")
+  noised_image = apply_noise(image, noise_pxl_count)
+  noised_image.save("noised_image.jpg")
