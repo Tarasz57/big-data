af982de33355e50a1e688705d2672803b1f5e983
Deividas Balandis <deividas.balandis@gmail.com>
2024-03-23 20:08:35 +0200

    adding README and performance count for separate processing steps

---
+++ README.md
@@ -0,0 +1,10 @@
+# 1st assignment
+
+Before starting the script you must do the following:
+
+- Install requirements: `pip install -r requirements.txt`
+- Download image data set into project directory: `wget https://www.dropbox.com/s/1eb7fx9bqr85cfo/data_set_VU_test1.zip`
+- Unzip it: `unzip data_set_VU_test1.zip`
+- Create 3 folders for processed image saving: `mkdir blacked && mkdir blurred && mkdir noised`
+
+Run the script with `python image.py`
--- image.py
+++ image.py
@@ -1,3 +1,4 @@
+from multiprocessing.managers import ListProxy
 from PIL import Image, ImageFilter 
 from skimage.util import random_noise, img_as_float
 import numpy as np
@@ -5,8 +6,10 @@ import multiprocessing as mp
 from os import listdir
 import time
 
+
 def apply_blur(image):
   return image.filter(ImageFilter.GaussianBlur)
+
 
 def apply_black_and_white(image: Image.Image):
   gray_scale = image.convert('L')
@@ -16,31 +19,61 @@ def apply_black_and_white(image: Image.Image):
   pixels_to_replace = sum(1 for pixel in pixels if pixel < thresh) / len(pixels) * 0.1
 
   fn = lambda x : 255 if x > thresh else 0
-  return gray_scale.point(fn, mode='1'), pixels_to_replace
+  blacked_image = gray_scale.point(fn, mode='1')
+  return blacked_image, pixels_to_replace
+
 
 def apply_noise(image: Image.Image, noise_pxl_count: float):
-  noised_image = random_noise(img_as_float(image), mode='s&p', amount=noise_pxl_count)
-  return Image.fromarray((noised_image * 255).astype(np.uint8))
+  noise = random_noise(img_as_float(image), mode='s&p', amount=noise_pxl_count)
+  return Image.fromarray((noise * 255).astype(np.uint8))
 
-def process_image(image_path: str):
+
+def find_average(times: ListProxy):
+  return sum(times) / len(times)
+
+
+def process_image(image_path: str, times_to_bw: list, times_to_noise: list, times_to_blur: list):
   print("Processing image: " + image_path)
   image = Image.open("Images/" + image_path)
+
+  time_to_bw = time.perf_counter()
   blacked_image, noise_pxl_count = apply_black_and_white(image)
+  time_to_bw = time.perf_counter() - time_to_bw
   blacked_image.save("blacked/" + image_path)
-  apply_noise(image, noise_pxl_count).save("noised/" + image_path)
-  apply_blur(image).save("blurred/" + image_path)
+
+  time_to_noise = time.perf_counter()
+  noised_image = apply_noise(image, noise_pxl_count)
+  time_to_noise = time.perf_counter() - time_to_noise
+  noised_image.save("noised/" + image_path)
+
+  time_to_blur = time.perf_counter()
+  blurred_image = apply_blur(image)
+  time_to_blur = time.perf_counter() - time_to_blur
+  blurred_image.save("blurred/" + image_path)
+
+  times_to_bw.append(time_to_bw)
+  times_to_noise.append(time_to_noise)
+  times_to_blur.append(time_to_blur)
+
 
 if __name__ == '__main__':
-  start = time.perf_counter()
+  times_to_bw = mp.Manager().list()
+  times_to_noise = mp.Manager().list()
+  times_to_blur = mp.Manager().list()
   pool = mp.Pool(mp.cpu_count())
 
   file_path = "Images/"
-  pool.map(process_image, [f for f in listdir(file_path)])
+  image_count = len(listdir(file_path))
+  start = time.perf_counter()
+  
+  pool.starmap(process_image, [(f, times_to_bw, times_to_noise, times_to_blur ) for f in listdir(file_path)])
 
+  end = time.perf_counter() - start
   pool.close()
   pool.join()
-
-# TODO - Add time measurement for each processing function individually
   
-  print("Finished in: " + str(time.perf_counter() - start) + " seconds")
+  print("Finished processing of " + str(image_count)+ " images" + " in: " + str(end) + " seconds")
+  print("Average time to black and white: " + str(find_average(times_to_bw)) + " seconds")
+  print("Average time to noise: " + str(find_average(times_to_noise)) + " seconds")
+  print("Average time to blur: " + str(find_average(times_to_blur)) + " seconds")
   
